712953b1ae2b61591bfddd7a40fda4ff8429d151
s00214097 <s00214097@atu.ie>
2023-04-09 15:22:12 +0100

    env updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,12 @@ export const environment = {
   //apiUri : 'http://localhost:3000',
   apiUri: 'https://osd2023backend.up.railway.app',
   //jwtDecoderUri: 'https://localhost:4200',
-  jwtDecoderUri: 'https://localhost:4200',
+  jwtDecoderUri: 'https://osd2023frontend.up.railway.app',
   auth0:
   {
     domain: 'dev-slwtm6bdvggyj45g.us.auth0.com',
     clientId: 'oGcxKCa3ImHiZHzQhwSXIeELHSghKEvT',
-    callback_URL: 'localhost:4200/callback',
+    callback_URL: 'https://osd2023frontend.up.railway.app/callback',
     audience: 'recipies'
   }
 };
